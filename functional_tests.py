aaa6e7d9dc7ce90996b92bbdb601d5dd8f69f2ad
jing Wang <admin@jingwangnet.com>
2022-04-18 16:20:59 +0800

    Extending our functional test using the unittest module.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import unittest
+import os
 
-browser = webdriver.Chrome()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+   
+    def setUp(self):
+        if os.environ.get('HEADLESS', False):
+            options = Options()
+            options.headless = True
+            self.browser = webdriver.Chrome(options=options)
+        else:
+            self.browser = webdriver.Chrome()
 
-assert 'successful' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_start_services_and_retrieve_it_later(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn("静网", self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
