f533db47d5e5958957729beeb8af14db86495ab9
jing Wang <admin@jingwangnet.com>
2022-04-18 16:50:30 +0800

    The "Dont test constants" rule, and template to the rescue.

--- services/tests.py
+++ services/tests.py
@@ -6,18 +6,8 @@ from .views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
     
-    def test_can_resolve_root_url_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        html = response.content.decode()
-
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>静网</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+    def test_use_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
 
