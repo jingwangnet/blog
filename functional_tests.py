4f18294ae39ff28a0010dadf8b9a51bab07b5a16
jing Wang <admin@jingwangnet.com>
2022-04-18 16:43:08 +0800

    Update FTs: FTs can check we input now."

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import unittest
 import os
+import time
 
 class NewVisitorTest(unittest.TestCase):
    
@@ -18,7 +20,37 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_start_services_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000')
+
         self.assertIn("静网", self.browser.title)
+        header = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn("静网", header)
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_service_name')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            "服务名"
+        )
+        submit = self.browser.find_element(By.ID, 'id_submit')
+        self.assertEqual(
+            inputbox.get_attribute('value'),
+            "提交"
+        )
+
+
+        inputbox.send_keys("Poptop")
+        submit.click()
+
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_service_table')
+        rows = tables.find_elements(By.TAG_NAME, 'tr')
+
+        self.assertIn(
+            '1. Poptop',
+            [row.name for row in rows]
+        )
+        
+
 
 
 if __name__ == '__main__':
