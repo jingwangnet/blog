3bd7809944a202edb2bc4c225f71f4e2bfd9de83
jing Wang <admin@jingwangnet.com>
2022-04-18 16:31:34 +0800

    Home page view return minimal content.

--- services/tests.py
+++ services/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.http import HttpRequest
 from django.urls import resolve
 from .views import home_page
 
@@ -8,3 +9,15 @@ class HomePageTest(TestCase):
     def test_can_resolve_root_url_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        html = response.content.decode()
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>静网</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+
