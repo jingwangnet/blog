24081b7941e4db0115fddd7dff60f854fd2ac207
jing Wang <admin@jingwangnet.com>
2022-04-18 18:27:31 +0800

    Django's ORM and our first model.

--- services/models.py
+++ services/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(verbose_name="服务名", max_length=20)
--- services/tests.py
+++ services/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.urls import resolve
 from .views import home_page
+from .models import Service
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -21,3 +22,20 @@ class HomePageTest(TestCase):
         self.assertIn('A service', html)
         self.assertTemplateUsed(response, 'home.html')
 
+
+class ServiceModelTest(TestCase):
+
+    def test_creating_and_saving_service(self):
+        service_1 = Service()
+        service_1.name = 'pptpd'
+        service_1.save()
+        service_2 = Service()
+        service_2.name = 'xl2tpd'
+        service_2.save()
+
+        self.assertEqual(2, Service.objects.count())
+
+        saved_service_1, saved_service_2 = Service.objects.all()
+
+        self.assertEqual(saved_service_1.name, 'pptpd')
+        self.assertEqual(saved_service_2.name, 'xl2tpd')
