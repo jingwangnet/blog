7cad6a564f50b2b225acd0d246d93c9c685b542c
jing Wang <admin@jingwangnet.com>
2022-04-19 12:23:26 +0800

    It does not have link of category in templates.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -174,7 +174,9 @@ class NewVisitorTest(LiveServerTestCase):
         page = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('pptpd', page)
 
-        self.browser.get(self.live_server_url)
+        home_link = self.browser.find_element(By.LINK_TEXT, '静网')
+        home_link.click()
+        #self.browser.get(self.live_server_url)
         page = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertIn('pptpd', page)
         self.assertIn('nps', page)
@@ -183,5 +185,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_text_in_dl('dd', '1. pptpd')
         self.check_text_in_dl('dt', 'NAT traversal')
         self.check_text_in_dl('dd', '1. nps')
+
         
+        vpn_link = self.browser.find_element(By.LINK_TEXT, 'Virtual Private Network')
+        vpn_link.click()
+        self.wait_to_check_text_in_table('1. pptpd')
+        self.check_text_in_element("Virtual Private Network", 'h1')
+        self.check_text_in_element("VPN", 'abbr')
+        self.check_text_in_element("将专用网络延伸到公共网络上，使用户能够在共享或公共网络上发送和接收数据，就像他们的计算设备直接连接到专用网络上一样[1]。VPN的好处包括增加专用网络的功能、安全性和管理，它提供了对公共网络上无法访问的资源访问通常用于远程办公人员。加密很常见但不是VPN连接的固有部分。", 'p')
+
 
